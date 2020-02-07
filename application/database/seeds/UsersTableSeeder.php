1f88f6b310cb0a586b75fbc16bccc8b074bac1ae
MechFroG88 <kelzzin2@gmail.com>
2020-02-07 14:16:43 +0800

    Update Seeder

--- application/database/seeds/UsersTableSeeder.php
+++ application/database/seeds/UsersTableSeeder.php
@@ -25,6 +25,20 @@ class UsersTableSeeder extends Seeder
             'cn_name' => '陈伟辰',
             'ic' => Hash::make('010527140579'),
             'class_id' => 0,
+        ],
+        [
+            'id' => 140204,
+            'type' => 0,
+            'cn_name' => '陈伟辰',
+            'ic' => Hash::make('010527140579'),
+            'class_id' => 0,
+        ],
+        [
+            'id' => 119999,
+            'type' => 0,
+            'cn_name' => '教务处',
+            'ic' => Hash::make('780326009682'),
+            'class_id' => 0
         ]
         ]);
     }
