43d26f2aeae9ede8e1562f6440c357a991fc320f
MechFroG88 <kelzzin2@gmail.com>
2020-02-07 10:43:58 +0800

    Update CORS

--- application/public/index.php
+++ application/public/index.php
@@ -35,6 +35,14 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
+if(isset($_SERVER['HTTP_ORIGIN'])){
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Headers: authorization, X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+    header("Access-Control-Max-Age: 1000");
+    header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+}
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
