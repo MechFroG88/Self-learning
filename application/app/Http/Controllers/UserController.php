004f97b08a8c177e28f9329513cb58cf7d4374ef
MechFroG88 <kelzzin2@gmail.com>
2020-02-17 11:57:39 +0800

    Update DateTime

--- application/app/Http/Controllers/UserController.php
+++ application/app/Http/Controllers/UserController.php
@@ -169,9 +169,9 @@ class UserController extends Controller
     public function submit(Request $data)
     {
         if (env('APP_ENV') == 'production'){
-            $now = new DateTime();
-            $start = DateTime::createFromFormat('Y-m-d H:i:s', '2020-02-18 20:00:00');   
-            $end = DateTime::createFromFormat('Y-m-d H:i:s', '2020-02-21 20:00:00'); 
+            $now = new \DateTime();
+            $start = \DateTime::createFromFormat('Y-m-d H:i:s', '2020-02-18 20:00:00');   
+            $end = \DateTime::createFromFormat('Y-m-d H:i:s', '2020-02-21 20:00:00'); 
             if ($now < $start) return $this->fail();
             if ($now > $end) return $this->fail();
         }
