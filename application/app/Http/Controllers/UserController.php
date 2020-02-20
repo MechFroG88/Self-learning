9a8f4fd86f07643841f98636b18c02613e63209e
MechFroG88 <kelzzin2@gmail.com>
2020-02-20 22:59:49 +0800

    Update Stream

--- application/app/Http/Controllers/UserController.php
+++ application/app/Http/Controllers/UserController.php
@@ -253,7 +253,7 @@ class UserController extends Controller
             $current_year = (int)(($user->classes->en_name)[0]);
             $not_allowed = true;
             if ($lesson->stream == '理' && $stream != '理') $not_allowed = false;
-            if ($lesson->stream == '文' && $stream != '文' && $stream != '商') $not_allowed = false;
+            if ($lesson->stream == '文' && ($stream == '文' || $stream == '商')) $not_allowed = false;
             if ($lesson->gender == '男' && $gender != '男') $not_allowed = false;
             if ($lesson->gender == '女' && $gender != '女') $not_allowed = false;
             foreach ($lesson->years as $year){
